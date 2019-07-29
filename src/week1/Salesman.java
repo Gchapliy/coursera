640068bb5b681bb6137847bf97eb90a0190d4ec1
Hryhorii_Chaplii <grishachapliy1@gmail.com>
2019-07-29 09:36:06 +0300

    week1

--- src/week1/Salesman.java
+++ src/week1/Salesman.java
@@ -2,34 +2,43 @@ package week1;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Salesman {
 
     static int n;
-    static int[][] a = { {0, 0}, {0, 1}, {0, 4}, {0, 6},
-                         {1, 0}, {1, 1}, {1, 4}, {1, 6},
-                         {2, 0}, {2, 1}, {2, 4}, {2, 6},
-                         {3, 0}, {3, 1}, {3, 4}, {3, 6},
+    static int[][] a = {
+            {0, 1, 4, 6},
+            {1, 0, 5, 2},
+            {4, 5, 0, 3},
+            {6, 2, 3, 0},
     };
     static int[] p;
     static boolean[] used;
     static int ans = Integer.MAX_VALUE;
 
     static void out() {
-
+        System.out.println(count());
+        System.out.println(Arrays.toString(p));
     }
 
     static int count() {
+        int count = 0;
 
-        return 0;
+        for (int i = 0; i < p.length; i++) {
+            if (i < p.length - 1) count += a[p[i]][p[i + 1]];
+            else count += a[p[i]][0];
+        }
+
+        return count;
     }
 
     static void rec(int idx, int len) {
         if (len >= ans)
             return;
         if (idx == n) {
-            int temp = count();
-            ans = ans < temp ? ans : temp;
+            int count = count();
+            ans = count < ans ? count : ans;
             out();
             return;
         }
@@ -45,10 +54,11 @@ public class Salesman {
     }
 
     public static void main(String[] args) {
-       /* n = 4;
+        n = 4;
         used = new boolean[n];
+        p = new int[n];
         p[0] = 0;
-        rec(1, 0);*/
-        System.out.println(a[0][1]);
+        rec(1, 0);
+
     }
 }
